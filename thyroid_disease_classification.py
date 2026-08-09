3b142ca0ee347ffc43f231a807396777e56ab5bf
Ana Farrando <160774664+AnaFarrando@users.noreply.github.com>
2026-08-10 01:58:28 +0200

    Add files via upload

---
+++ thyroid_disease_classification.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+import argparse
+import lzma
+import os
+import pickle
+import sys
+from typing import Optional
+import urllib.request
+
+import numpy as np
+import numpy.typing as npt
+
+import sklearn.linear_model
+import sklearn.preprocessing
+import sklearn.pipeline
+import sklearn.model_selection
+
+parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
+parser.add_argument("--predict", default=None, type=str, help="Path to the dataset to predict")
+parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
+parser.add_argument("--seed", default=42, type=int, help="Random seed")
+# For these and any other arguments you add, ReCodEx will keep your default value.
+parser.add_argument("--model_path", default="thyroid_competition.model", type=str, help="Model path")
+
+
+class Dataset:
+    """Thyroid Dataset.
+
+    The dataset contains real medical data related to thyroid gland function,
+    classified either as normal or irregular (i.e., some thyroid disease).
+    The data consists of the following features in this order:
+    - 15 binary features
+    - 6 real-valued features
+
+    The target variable is binary, with 1 denoting a thyroid disease and
+    0 normal function.
+    """
+    def __init__(self,
+                 name="thyroid_competition.train.npz",
+                 url="https://ufal.mff.cuni.cz/~courses/npfl129/2526/datasets/"):
+        if not os.path.exists(name):
+            print("Downloading dataset {}...".format(name), file=sys.stderr)
+            urllib.request.urlretrieve(url + name, filename="{}.tmp".format(name))
+            os.rename("{}.tmp".format(name), name)
+
+        # Load the dataset and return the data and targets.
+        dataset = np.load(name)
+        for key, value in dataset.items():
+            setattr(self, key, value)
+
+
+def main(args: argparse.Namespace) -> Optional[npt.ArrayLike]:
+    if args.predict is None:
+        # We are training a model.
+        np.random.seed(args.seed)
+        train = Dataset()
+
+        # TODO: Train a model on the given dataset and store it in `model`.
+        pipeline = sklearn.pipeline.Pipeline([ # Create pipeline with scaler, polynomial features, and logistic regression
+            ("poly", sklearn.preprocessing.PolynomialFeatures(include_bias=False)),
+            ("scaler", sklearn.preprocessing.StandardScaler()),
+            ("logr", sklearn.linear_model.LogisticRegression(max_iter=5000, random_state=args.seed))
+        ])
+
+        param_grid = { # Define parameter grid for grid search
+        'poly__degree': [1, 2],
+        'logr__C': [0.01, 0.1, 1, 10, 100],
+        'logr__solver': ['lbfgs']
+        }
+
+        skf = sklearn.model_selection.StratifiedKFold(n_splits=5, shuffle=True, random_state=args.seed) # Define stratified k-fold cross-validator with 5 folds
+        grid = sklearn.model_selection.GridSearchCV(pipeline, param_grid, cv=skf, scoring="accuracy", n_jobs=-1, verbose=0)
+
+        # Fit and find the best model
+        grid.fit(train.data, train.target)
+
+        model = grid.best_estimator_
+        model.fit(train.data, train.target)
+
+        # Serialize the model.
+        with lzma.open(args.model_path, "wb") as model_file:
+            pickle.dump(model, model_file)
+
+    else:
+        # Use the model and return test set predictions, either as a Python list or a NumPy array.
+        test = Dataset(args.predict)
+
+        with lzma.open(args.model_path, "rb") as model_file:
+            model = pickle.load(model_file)
+
+        # TODO: Generate `predictions` with the test set predictions.
+        predictions = model.predict(test.data)
+
+        return predictions
+
+
+if __name__ == "__main__":
+    main_args = parser.parse_args([] if "__file__" not in globals() else None)
+    main(main_args)
